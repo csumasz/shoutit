79734d1ee48d6711865f635c3d80f849e113bdd1
Varga Csaba <33159258+csumasz@users.noreply.github.com>
2023-11-14 17:25:40 +0100

    Update test.php

--- test.php
+++ test.php
@@ -32,7 +32,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Shout It</title>
-    <link rel="stylesheet" href="css/test.css">
+    <link rel="stylesheet" href="test.css">
 </head>
 <body>
     <div id="container">
